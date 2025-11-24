3e9da14a51a99097c52bc86c5109a172f3063e0b
Joan Janzelly <joancefet@users.noreply.github.com>
2025-11-24 12:15:15 -0300

    Modify app.run to use PORT environment variable
    Update app to use environment variable for port

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import re
 from collections import defaultdict
 
@@ -575,4 +576,5 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
